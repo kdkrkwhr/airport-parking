17c4e069df73f8f840056127c17c76679c79f6a6
keepConcentration <rngnsdms123@naver.com>
2021-03-13 12:13:26 +0900

    [feat] Redis 연동 추가

--- src/main/java/com/kdk/util/CommonConstant.java
+++ src/main/java/com/kdk/util/CommonConstant.java
@@ -11,4 +11,18 @@ abstract public class CommonConstant {
 
   public static final String OPEN_API_DOMAIN = "http://openapi.airport.co.kr";
   public static final String OPEN_API_TYPE = "/service/rest/AirportParkingCongestion/airportParkingCongestionRT";
+  
+  public static final String REDIS_PREFIX_INFO = "INFO_";
+  public static final String REDIS_PREFIX_FLAG = "FLAG_";
+  public static final String REDIS_PREFIX_TIME = "TIME_";
+
+  public static final String REDIS_SUFFIX_ALERT = "_ALERT";
+  public static final String REDIS_SUFFIX_PM10 = "_PM10";
+  public static final String REDIS_SUFFIX_PM25 = "_PM25";
+  public static final String REDIS_SUFFIX_CO2 = "_CO2";
+  public static final String REDIS_SUFFIX_VOC = "_VOC";
+  public static final String REDIS_SUFFIX_TEMP = "_TEMP";
+  public static final String REDIS_SUFFIX_HUMI = "_HUMI";
+
+  public static final String REDIS_NO_DATA = "NO_DATA";
 }
